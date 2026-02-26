0a27468e14f69d08c19051458ab3ff8003f5c079
itsmehlondyn <itsmehlondyn@gmail.com>
2026-02-25 20:30:08 -0500

    fixed name

--- CSCI271_Assignment2_QuestionOneLondynPepperdine.java
+++ CSCI271_Assignment2_QuestionOneLondynPepperdine.java
@@ -32,7 +32,7 @@
 ********************************************************************/
 import java.util.Scanner;
 
-public class RecursionQuestionOne {
+public class CSCI271_Assignment2_QuestionOneLondynPepperdine {
 /*****************************stringName********************************
 * Description: Recursively prints each character of a string starting
 *              from a given index and calculates the total length.
